a339a466ec56e59ece4c4ed8c09c6057978a2f0a
Leonardo Amaral <leonardo_amaral98@hotmail.com>
2025-02-15 22:46:18 -0300

    Feature: Adciona novas funções na biblioteca UIUtils para criar botões e labels, evitando repetição de código

--- Contents/mods/safehouse_plus/media/lua/client/ISUI/SP_UIUtils.lua
+++ Contents/mods/safehouse_plus/media/lua/client/ISUI/SP_UIUtils.lua
@@ -28,4 +28,23 @@ function UIUtils.highlightSquare(sq, r, g, b, a)
     end
 end
 
+-- Cria e adiciona um botão a um objeto de UI.
+function UIUtils.createButton(object, x, y, width, height, text, onClick)
+    local button = ISButton:new(x, y, width, height, text, object, onClick)
+    button:initialise()
+    button:instantiate()
+    object:addChild(button)
+    return button
+end
+
+-- Cria e adiciona um label a um objeto de UI.
+function UIUtils.createLabel(object, x, y, height, text, font, color, alignLeft, center)
+    local label = ISLabel:new(x, y, height, text, color.r, color.g, color.b, color.a, font, alignLeft)
+    label.center = center or false
+    label:initialise()
+    label:instantiate()
+    object:addChild(label)
+    return label
+end
+
 return UIUtils
